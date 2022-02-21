f78c5a19cf16254767ab98b637b850572a127807
partho2589 <parthomalo615@.com>
2022-02-21 17:07:49 +0600

    arrow file

---
+++ arrow.js
@@ -0,0 +1,10 @@
+// const array = [25, 22, 15, 90, 46, 222, 55, 13];
+// const sqears = array.map(num => num/5);
+// console.log(sqears)
+
+const add = (num1, num2) => (num1+2) * (num2 +2);
+// console.log(add(8, 18))
+
+
+const threeAdd = (n1, n2 , n3) => n1 * n2 * n3;
+console.log(threeAdd(10, 20, 10))
